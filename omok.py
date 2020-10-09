8959623f3ca15269502cc55ac35da85d294a9bc6
syki66 <66syki@gmail.com>
2020-10-09 19:17:02 +0900

    rename variables

--- omok.py
+++ omok.py
@@ -6,7 +6,6 @@ import math
 #배경
 wn = turtle.Screen()
 wn.bgcolor("yellow")
-wn.title("오목 made by syki66")
 wn.setup(1000,1000)
 
 #장기판 셋업
@@ -20,7 +19,7 @@ border.pensize(4)
 border.lt(90)
 
 #배열, 판정때문에 크게만듬
-ffff = [[0]*22 for i in range(22)]
+checkArray = [[0]*22 for i in range(22)]
 
 #세로줄
 for i in range(17):
@@ -38,32 +37,32 @@ for j in range(17):
     border.fd(800)
 
 #판정
-def logic(a,b):
-    for p in range(5):
+def isBlackWin(a,b):
+    for i in range(5):
         #흑승리
-        if ffff [a+p][b] == 1 and ffff [a-1+p][b] == 1 and ffff [a-2+p][b] == 1 and ffff[a-3+p][b] == 1 and ffff[a-4+p][b] == 1:
+        if checkArray [a+i][b] == 1 and checkArray [a-1+i][b] == 1 and checkArray [a-2+i][b] == 1 and checkArray[a-3+i][b] == 1 and checkArray[a-4+i][b] == 1:
             return True
-        if ffff [a][b+p] == 1 and ffff [a][b-1+p] == 1 and ffff [a][b-2+p] == 1 and ffff[a][b-3+p] == 1 and ffff[a][b-4+p] == 1 :
+        if checkArray [a][b+i] == 1 and checkArray [a][b-1+i] == 1 and checkArray [a][b-2+i] == 1 and checkArray[a][b-3+i] == 1 and checkArray[a][b-4+i] == 1 :
             return True
-        if ffff [a+p][b+p] == 1 and ffff [a-1+p][b-1+p] == 1 and ffff [a-2+p][b-2+p] == 1 and ffff[a-3+p][b-3+p] == 1 and ffff[a-4+p][b-4+p] == 1:
+        if checkArray [a+i][b+i] == 1 and checkArray [a-1+i][b-1+i] == 1 and checkArray [a-2+i][b-2+i] == 1 and checkArray[a-3+i][b-3+i] == 1 and checkArray[a-4+i][b-4+i] == 1:
             return True
-        if ffff [a+p][b-p] == 1 and ffff [a-1+p][b+1-p] == 1 and ffff [a-2+p][b+2-p] == 1 and ffff[a-3+p][b+3-p] == 1 and ffff[a-4+p][b+4-p] == 1:
+        if checkArray [a+i][b-i] == 1 and checkArray [a-1+i][b+1-i] == 1 and checkArray [a-2+i][b+2-i] == 1 and checkArray[a-3+i][b+3-i] == 1 and checkArray[a-4+i][b+4-i] == 1:
             return True
         #백승리
-        if ffff [a+p][b] == 2 and ffff [a-1+p][b] == 2 and ffff [a-2+p][b] == 2 and ffff[a-3+p][b] == 2 and ffff[a-4+p][b] == 2:
+        if checkArray [a+i][b] == 2 and checkArray [a-1+i][b] == 2 and checkArray [a-2+i][b] == 2 and checkArray[a-3+i][b] == 2 and checkArray[a-4+i][b] == 2:
             return False
-        if ffff [a][b+p] == 2 and ffff [a][b-1+p] == 2 and ffff [a][b-2+p] == 2 and ffff[a][b-3+p] == 2 and ffff[a][b-4+p] == 2 :
+        if checkArray [a][b+i] == 2 and checkArray [a][b-1+i] == 2 and checkArray [a][b-2+i] == 2 and checkArray[a][b-3+i] == 2 and checkArray[a][b-4+i] == 2 :
             return False
-        if ffff [a+p][b+p] == 2 and ffff [a-1+p][b-1+p] == 2 and ffff [a-2+p][b-2+p] == 2 and ffff[a-3+p][b-3+p] == 2 and ffff[a-4+p][b-4+p] == 2:
+        if checkArray [a+i][b+i] == 2 and checkArray [a-1+i][b-1+i] == 2 and checkArray [a-2+i][b-2+i] == 2 and checkArray[a-3+i][b-3+i] == 2 and checkArray[a-4+i][b-4+i] == 2:
             return False
-        if ffff [a+p][b-p] == 2 and ffff [a-1+p][b+1-p] == 2 and ffff [a-2+p][b+2-p] == 2 and ffff[a-3+p][b+3-p] == 2 and ffff[a-4+p][b+4-p] == 2:
+        if checkArray [a+i][b-i] == 2 and checkArray [a-1+i][b+1-i] == 2 and checkArray [a-2+i][b+2-i] == 2 and checkArray[a-3+i][b+3-i] == 2 and checkArray[a-4+i][b+4-i] == 2:
             return False
 
 #키보드입력
-def ClickSpace():
+def spaceClicked():
     for i in range(22):
         for j in range(22):
-            ffff[i][j] = 0
+            checkArray[i][j] = 0
     turtle.clearscreen()
 
     wn.bgcolor("yellow")
@@ -89,47 +88,19 @@ def ClickSpace():
         border.pendown()
         border.fd(800)
 
-    turtle.color("blue")
-    turtle.hideturtle()
-    turtle.speed(0)
-    turtle.penup()
-    turtle.setposition(150,-450)
-    turtle.write("made by syki66", False, font=("Arial", 30, "bold"))
+    m.onclick(run_omok)
 
-    m.onclick(aaa)
 
-#누가만들었나
-turtle.color("blue")
-turtle.hideturtle()
-turtle.speed(0)
-turtle.penup()
-turtle.setposition(-20,-460)
-turtle.write("오목 v1.0 made by syki66", False, font=("Arial", 30, "bold"))
 
 #2인용 오목
 count = 0
-def aaa(xclick,yclick):
+def run_omok(xClick,yClick):
     global count
-    #수계산(렉걸려서 빼놓음)
-    '''t2=turtle.Turtle()
-    t2.penup()
-    t2.speed(0)
-    t2.setposition(0,510)
-    t2.color("yellow")
-    t2.shape("square")
-    t2.shapesize(10,10)
-    
-    t3=turtle.Turtle()
-    t3.hideturtle()
-    t3.speed(0)
-    t3.penup()
-    t3.setposition(-100,400)
-    t3.write(str(count+1)+"수", False, font=("Arial", 70, "bold"))'''
     
     for i in range(17):
         for j in range(17):
-            distance = math.sqrt(math.pow(xclick-(-400+(i*50)),2)+math.pow(yclick-(-400+(j*50)),2))
-            if distance < 25 and ffff[i][j] == 0:
+            distance = math.sqrt(math.pow(xClick-(-400+(i*50)),2)+math.pow(yClick-(-400+(j*50)),2))
+            if distance < 25 and checkArray[i][j] == 0:
                 a1=turtle.Turtle()
                 a1.penup()
                 a1.speed(0)
@@ -139,13 +110,13 @@ def aaa(xclick,yclick):
                 
                 if count % 2 == 1:
                     a1.color("white")
-                    ffff[i][j] = 2
+                    checkArray[i][j] = 2
                 else:
                     a1.color("black")
-                    ffff[i][j] = 1
+                    checkArray[i][j] = 1
                                 
-                #print(ffff)
-                if logic(i,j) == True:
+                #print(checkArray)
+                if isBlackWin(i,j) == True:
                     print("흑 승")
                     turtle.color("black")
                     turtle.hideturtle()
@@ -159,11 +130,11 @@ def aaa(xclick,yclick):
                     count = -1
                     for i in range(22):
                         for j in range(22):
-                            ffff[i][j] = 1
+                            checkArray[i][j] = 1
                     turtle.listen()
-                    turtle.onkey(ClickSpace, "space")
+                    turtle.onkey(spaceClicked, "space")
                                                
-                elif logic(i,j) == False:
+                elif isBlackWin(i,j) == False:
                     print("백 승")
                     turtle.color("white")
                     turtle.hideturtle()
@@ -177,9 +148,9 @@ def aaa(xclick,yclick):
                     count = -1
                     for i in range(22):
                         for j in range(22):
-                            ffff[i][j] = 1
+                            checkArray[i][j] = 1
                     turtle.listen()
-                    turtle.onkey(ClickSpace, "space")
+                    turtle.onkey(spaceClicked, "space")
                 
                 count += 1
                 print(count)
@@ -189,7 +160,7 @@ def aaa(xclick,yclick):
                 pass
 
 m = turtle.Screen()
-m.onclick(aaa)
+m.onclick(run_omok)
 
 #콘솔창 자동으로 안꺼지게
 turtle.mainloop()
